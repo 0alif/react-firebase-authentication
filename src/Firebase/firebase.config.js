c39533fbd7b2cb226bc0bba2c8da2610a19446d5
0alif <alifhossain003@gmail.com>
2021-10-07 21:24:37 +0600

    first commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDcaWSo8dRScPCgHUz10K-Om-jv-6hpcGI",
+    authDomain: "react-firebase-authentic-1e45b.firebaseapp.com",
+    projectId: "react-firebase-authentic-1e45b",
+    storageBucket: "react-firebase-authentic-1e45b.appspot.com",
+    messagingSenderId: "96498224556",
+    appId: "1:96498224556:web:4fb9a4c9e8d7f51ae9fe09"
+};
+
+export default firebaseConfig;
